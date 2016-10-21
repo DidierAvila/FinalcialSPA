d25b2bb755297883cf286f13247792ee949f294f
DidierAvila <didier_avila9c@hotmail.com>
2016-10-21 00:29:50 -0500

    Cambios usuario
    Cambios usuario

---
+++ Negocio/Usuario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos;
+
+namespace Negocio
+{
+    public class Usuario
+    {
+        private string Username;
+        private string Password;
+
+        public string _Password
+        {
+            get { return Password; }
+            set { Password = value; }
+        }
+        
+        public string _Username
+        {
+            get { return Username; }
+            set { Username = value; }
+        }
+
+
+        public bool IniciarSesion()
+        {
+            return true;
+        }
+    }
+}
